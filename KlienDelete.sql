d6c9e0024a4c7da8bb50cfbf8420c6bf8046e7ce
friska christiana <christianafriska@gmail.com>
2020-04-29 15:44:26 +0700

    klienDelete
    ga paham gbs masuk history

--- KlienDelete.sql
+++ KlienDelete.sql
@@ -1,20 +1,21 @@
+--select * from klien
+--select * from hubungan
+--select * from region
+--select * from noHpklien
+--select * from perubahan
+--select * from history
 ALTER PROCEDURE KlienDelete(
 @nama VARCHAR(50)
 )
 AS
 	DECLARE 
 		@idklien INT,
+		@idRecordHub INT,
 		@curDateTime DATETIME,
 		@idRecord INT,
-		@idPerubahan INT
+		@idPerubahan INT,
+		@idPerubahanHub INT
 	SET @curDateTime = GETDATE()
-
-	SELECT
-		idK = @idklien
-	FROM
-		klien
-	WHERE
-		nama = @nama
 
 	SET @idRecord = (
 		SELECT
@@ -24,27 +25,54 @@ AS
 		WHERE
 			nama = @nama
 	)
+	/*
+	SET @idRecordHub = (
+		SELECT
+			hubungan.idKK
+		FROM
+			hubungan join klien on
+			hubungan.idKK = klien.fkHubungan
+		WHERE
+			klien.nama = @nama
+	)
+	*/
 	
 	INSERT INTO perubahan (waktu, tabel, idRecord, operasi)
-	VALUES (@curDateTime, 'klien', @idRecord, 'DELETE')
-
+	VALUES (@curDateTime, 'klien', @idRecord, '3')
+	/*
+	INSERT INTO perubahan (waktu, tabel, idRecord, operasi)
+	VALUES (@curDateTime, 'hubungan', @idRecordHub, '3')
+	*/
 	SET @idPerubahan = (
 		SELECT
 			idPe
 		FROM
 			Perubahan
 		WHERE
-			idRecord = @idRecord AND
-			tabel = 'klien'
+			idRecord = @idRecord AND tabel = 'klien'
 	)
-
+	/*
+	SET @idPerubahanHub = (
+		SELECT
+			idPe
+		FROM
+			perubahan
+		WHERE
+			idRecord = @idRecordHub AND tabel = 'hubungan'
+	)
+	*/
 	INSERT INTO history (fkPerubahan, kolom, tipeData, nilaiSebelum)
-	VALUES (@idPerubahan, 'nama', 'varchar(50)',@nama)
-
+	VALUES (@idPerubahan, 'nama', 'varchar(50)', @nama)
+	/*
+	INSERT INTO history (fkPerubahan, kolom, tipeData, nilaiSebelum)
+	VALUES (@idPerubahanHub, 'idUser', 'int', @idRecord)
+	*/
 	DELETE FROM klien
 	WHERE nama = @nama
-
-
+	/*
+	DELETE FROM hubungan
+	WHERE idUser = @idRecordHub
+	*/
 	SELECT
 		*
 	FROM
@@ -67,4 +95,4 @@ AS
 		nilaiSebelum AS 'Data klien Sebelum'
 	FROM history where kolom = 'nama'
 	go
-	exec KlienDelete 'sapi'
+	exec KlienDelete 'tine'
