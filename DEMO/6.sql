492e8dd0572507d8b62320ea79ff74316663b45d
denisestevani <56673734+denisestevani@users.noreply.github.com>
2020-05-28 18:36:50 +0700

    sp demo tanggal 3 dan 6

---
+++ DEMO/6.sql
@@ -0,0 +1,12 @@
+
+EXEC inverstasiUpdate 'Jarjat', 'cemara', '19980211', 'Bandung', 600000, 1
+UPDATE Perubahan SET waktu = "20200106 11:11:11 AM" where idRecord =  AND tabel = 'Investasi'
+UPDATE investasi SET waktu = "20200106 11:11:11 AM" where idInvest = 
+
+EXEC inverstasiUpdate 'Mail', 'batikayu', '19930704', 'Jakarta Selatan', 500000, 3
+UPDATE Perubahan SET waktu = "20200106 12:56:00 PM" where idRecord =  AND tabel = 'Investasi'
+UPDATE investasi SET waktu = "20200106 12:56:00 PM" where idInvest = 
+ 
+EXEC inverstasiUpdate 'Ehsan', 'kembartengah', '19970314', 'Jakarta Barat', 500000, 2
+UPDATE Perubahan SET waktu = "20200106 13:46:01 PM" where idRecord =  AND tabel = 'Investasi'
+UPDATE investasi SET waktu = "20200106 13:46:01 PM" where idInvest = 
