ea451c5d1e7ef3268cb48a96c1667abb314d3ffa
Cristine <christinekhong8@gmail.com>
2020-05-29 14:39:37 +0700

    update tgl7 fix investasi delete

--- DEMO/Tgl7.sql
+++ DEMO/Tgl7.sql
@@ -13,7 +13,7 @@ AS
 	UPDATE perubahan SET waktu ='20200107 10:40:00 AM' WHERE idRecord = 3 AND tabel = 'investasi' AND operasi = 'UPDATE'
 	UPDATE investasi SET waktu ='20200106 10:40:00 AM' WHERE fkIdKlien = 3
 
-	EXEC investasiDelete 8,3
+	EXEC KlienDelete 'Fizzi','kembartengah','19960120','Jakarta Barat',3
 	UPDATE investasi SET waktu ='20200107 11:10:00 AM' WHERE fkIdKlien = 8
 	UPDATE perubahan SET waktu ='20200107 11:10:00 AM' WHERE idRecord = 8 AND tabel = 'hubungan' AND operasi = 'DELETE'
 	UPDATE perubahan SET waktu ='20200107 11:10:00 AM' WHERE idRecord = 8 AND tabel = 'investasi' AND operasi = 'DELETE'
@@ -26,13 +26,13 @@ AS
 	EXEC insertRegion 'Palembang','Sumatera Selatan'
 	UPDATE perubahan SET waktu ='20200102 11:55:00 AM' WHERE idRecord = 25 AND tabel = 'Region' 
 
-	EXEC investasiDelete 6,2
+	EXEC KlienDelete 'Mail','batikayu','19930704','Jakarta Selatan',2
 	UPDATE investasi SET waktu ='20200107 13:30:00 AM' WHERE fkIdKlien = 6
 	UPDATE perubahan SET waktu ='20200107 13:30:00 AM' WHERE idRecord = 6 AND tabel = 'hubungan' AND operasi = 'DELETE'
 	UPDATE perubahan SET waktu ='20200107 13:30:00 AM' WHERE idRecord = 6 AND tabel = 'investasi' AND operasi = 'DELETE'
 	UPDATE perubahan SET waktu ='20200107 13:30:00 AM' WHERE idRecord = 6 AND tabel = 'klien' AND operasi = 'DELETE'
 
-	EXEC investasiDelete 4,3
+	EXEC KlienDelete 'Rose','cemara','19701210','Bandung',3
 	UPDATE investasi SET waktu ='20200107 13:45:00 AM' WHERE fkIdKlien = 4
 	UPDATE perubahan SET waktu ='20200107 13:45:00 AM' WHERE idRecord = 4 AND tabel = 'hubungan' AND operasi = 'DELETE'
 	UPDATE perubahan SET waktu ='20200107 13:45:00 AM' WHERE idRecord = 4 AND tabel = 'investasi' AND operasi = 'DELETE'
