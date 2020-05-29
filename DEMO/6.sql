abe91c4ebce76a94eb5664af09b1ccd09dd8ea1f
denisestevani <56673734+denisestevani@users.noreply.github.com>
2020-05-29 21:11:05 +0700

    perubahan 3 dan 6

--- DEMO/6.sql
+++ DEMO/6.sql
@@ -1,12 +1,12 @@
 
 EXEC inverstasiUpdate 'Jarjat', 'cemara', '19980211', 'Bandung', 600000, 1
-UPDATE Perubahan SET waktu = "20200106 11:11:11 AM" where idRecord =  AND tabel = 'Investasi'
-UPDATE investasi SET waktu = "20200106 11:11:11 AM" where idInvest = 
+UPDATE Perubahan SET waktu = "20200106 11:11:11 AM" where idRecord = 39  AND tabel = 'Investasi'
+UPDATE investasi SET waktu = "20200106 11:11:11 AM" where fkIdKlien = 21
 
 EXEC inverstasiUpdate 'Mail', 'batikayu', '19930704', 'Jakarta Selatan', 500000, 3
-UPDATE Perubahan SET waktu = "20200106 12:56:00 PM" where idRecord =  AND tabel = 'Investasi'
-UPDATE investasi SET waktu = "20200106 12:56:00 PM" where idInvest = 
+UPDATE Perubahan SET waktu = "20200106 12:56:00 PM" where idRecord = 40 AND tabel = 'Investasi'
+UPDATE investasi SET waktu = "20200106 12:56:00 PM" where fkIdKlien = 22
  
 EXEC inverstasiUpdate 'Ehsan', 'kembartengah', '19970314', 'Jakarta Barat', 500000, 2
-UPDATE Perubahan SET waktu = "20200106 13:46:01 PM" where idRecord =  AND tabel = 'Investasi'
-UPDATE investasi SET waktu = "20200106 13:46:01 PM" where idInvest = 
+UPDATE Perubahan SET waktu = "20200106 13:46:01 PM" where idRecord = 41 AND tabel = 'Investasi'
+UPDATE investasi SET waktu = "20200106 13:46:01 PM" where fkIdKlien = 23
